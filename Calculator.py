345a375dd4430f93df6102c1ef68ef19f0d8193e
lewisweru <codecrafters.mail.com>
2025-02-10 13:56:33 +0300

    Added a greeting function + squareroot and square function .

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,11 @@
 # CODE CRAFTERS PRESENTATION
+import math
+
+def greeting():
+    userName=input("Enter your name :")
+    if not userName:
+        userName = "user"
+    print(f"Hello {userName}")
 
 def add(a, b):
     return a + b
@@ -14,19 +21,38 @@ def divide(a, b):
         return "Error: Division by zero"
     return a / b
 
+def Squareroot(a):
+    if a < 0:
+        return "an Error! You can't find squareroots of negative numbers . "
+    return math.sqrt(a)
+def power(a,b):
+    return pow(a,b)
+
+
 def calculator():
+    greeting()
     while True:
-        print("\nSimple Calculator")
+        
+        print("\nEnjoy the Simple Calculator")
         print("Select operation:")
         print("1. Add")
         print("2. Subtract")
         print("3. Multiply")
         print("4. Divide")
-        print("5. Exit")
+        print("5. Squareroot")
+        print("6 Raise to power")
+        print("7. Exit")
         
-        choice = input("Enter choice (1/2/3/4/5): ")
+        choice = input("Enter choice (1/2/3/4/5/6/7): ")        
         
-        if choice == '5':
+        if choice in ('5'):
+            num1 = int(input("Enter a number to find its squareroot :"))
+            print(f" The squareroot of {num1} is {Squareroot(num1)}")
+        if choice in ('6'):
+            num1 = int(input("Enter the number you wish to raise to a power :"))
+            num2 =int(input("Enter the power :"))
+            print(f"The number {num1}, raised to power {num2} is {power(num1,num2)}")
+        if choice == '7':
             print("Exiting calculator. Goodbye!")
             break
         
@@ -39,16 +65,19 @@ def calculator():
                     '1': add,
                     '2': subtract,
                     '3': multiply,
-                    '4': divide
+                    '4': divide,
+                    #'5': Squareroot,
+                    #'6': power
                 }
                 
                 result = operations[choice](num1, num2)
                 print(f"Result: {result}")
             except ValueError:
                 print("Invalid input. Please enter numeric values.")
-        else:
+        elif not choice in ('1','2','3','4','5','6','7'):
+            
             print("Invalid choice. Please select a valid option.")
 
-if __name__ == "__main__":
-    calculator()
+
+calculator()
 
